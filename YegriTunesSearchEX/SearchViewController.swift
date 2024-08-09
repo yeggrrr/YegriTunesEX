7d8328b877e32ca82bd502ff29998acd7f4dca9a
yeggrrr <yjkim981103@gmail.com>
2024-08-09 12:14:15 +0900

    [Setting] 기본 셋팅

--- YegriTunesSearchEX/SearchViewController.swift
+++ YegriTunesSearchEX/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  YegriTunesSearchEX
 //
 //  Created by YJ on 8/9/24.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
 

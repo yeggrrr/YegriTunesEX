bfdbe4426ea400c7f0796d70717c5ba542a3d303
yeggrrr <yjkim981103@gmail.com>
2024-08-12 16:21:34 +0900

    [Change] Observable Single 활용

--- YegriTunesSearchEX/Sources/Network/NetworkManager.swift
+++ YegriTunesSearchEX/Sources/Network/NetworkManager.swift
@@ -9,33 +9,27 @@ import Foundation
 import RxSwift
 import Alamofire
 
-enum APIError: Error {
-    case invalidURL
-    case unknownResponse
-    case statusError
-}
-
 final class NetworkManager {
     static let shared = NetworkManager()
     private init() { }
     
-    func callRequest(query: String) -> Observable<SearchModel> {
+    func callRequest(query: String) -> Single<SearchModel> {
         let url = "https://itunes.apple.com/search?term=\(query)&entity=audiobook"
         
-        return Observable.create { observer in
+        return Single.create { observer in
             AF.request(url)
                 .validate(statusCode: 200..<300)
                 .responseDecodable(of: SearchModel.self) { response in
                     switch response.result {
                     case .success(let success):
-                        observer.onNext(success)
-                        observer.onCompleted()
+                        observer(.success(success))
                     case .failure(let error):
-                        observer.onError(error)
+                        observer(.failure(error))
                     }
                 }
             
             return Disposables.create()
         }
+        .debug("네트워크 통신")
     }
 }
